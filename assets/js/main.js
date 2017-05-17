c7611d175c0b3519d487831f0a7c89e77a863a83
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-17 19:47:06 -0400

    hola mundo al hacer click en el boton y alert de cuando la pagina cargo

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function escucha(){
+    var boton = document.getElementById("demo");
+    boton.addEventListener("click", function(){
+        alert("¡Hola mundo!");
+    });
+}
+escucha();
+
+function cargada(){
+    alert("la pagina cargo correctamente");
+}
